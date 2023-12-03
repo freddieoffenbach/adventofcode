e88839c4619c17307545b80d1349c4c336b2aa58
Freddie Offenbach <freddie.offenbach@gmail.com>
2023-12-03 01:34:06 +0100

    Update day 3 part 1

--- 2022/03-rucksack_reorganization/part_one.py
+++ 2022/03-rucksack_reorganization/part_one.py
@@ -2,19 +2,15 @@ with open('input.txt', 'r') as file:
     lines = file.readlines()
 
 sum = 0
-types = list('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
-priorites = []
-for n in range(1, 53):
-    priorites.append(n)
-items = dict(zip(types, priorites))
 
 for line in lines:
-    x = int(len(line) / 2)
-    comp1 = line[:x]
-    comp2 = line[x:]
-    for c in comp1:
-        if c in comp2:
-            sum += items[c]
+    x = len(line) // 2
+    for c in line[:x]:
+        if c in line[x:]:
+            if c.islower():
+                sum += ord(c) - 96
+            else:
+                sum += ord(c) - 38
             break
 
 print(sum) # 7763
