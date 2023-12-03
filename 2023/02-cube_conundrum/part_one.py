fe4b1733186f9d8fd69f6dfa5e261e6421cc7281
Freddie Offenbach <freddie.offenbach@gmail.com>
2023-12-03 01:58:52 +0100

    Update day 2 part 1

--- 2023/02-cube_conundrum/part_one.py
+++ 2023/02-cube_conundrum/part_one.py
@@ -9,8 +9,8 @@ for line in lines:
     id, sets = line[5:].split(':')
     for set in sets.split(';'):
         for cube in set.split(','):
-            n, color = cube.split()
-            if int(n) > bag.get(color):
+            n, c = cube.split()
+            if int(n) > bag.get(c):
                 is_possible = False
     if is_possible:
         sum += int(id)
